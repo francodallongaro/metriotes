4278e9311c81abf5b5686f5ee672b62b5cc66c41
franfiori29 <franfiori@gmail.com>
2021-03-12 15:00:30 -0300

    added refetch

--- src/components/inspiracion/index.js
+++ src/components/inspiracion/index.js
@@ -5,22 +5,23 @@ import SidebarQuotes from "../SidebarQuotes";
 function Inspiracion() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [refetch, setRefetch] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://goquotes-api.herokuapp.com/api/v1/random?count=10")
       .then((res) => res.json())
       .then((data) => {
         setQuotes(data.quotes);
         setLoading(false);
-        console.log("quotes", quotes);
       });
-  }, []);
+  }, [refetch]);
 
   if (loading) return <h1>Loading</h1>;
   return (
     <div className="layout">
       <SidebarQuotes />
-      <Quotes quotes={quotes} />
+      <Quotes quotes={quotes} setRefetch={setRefetch} />
     </div>
   );
 }
--- src/components/quotes/index.js
+++ src/components/quotes/index.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 
-function Quotes({ quotes }) {
+function Quotes({ quotes, setRefetch }) {
   const [contador, setContador] = useState(0);
+
+  const handleClick = () => {
+    if (contador === quotes.length - 1) {
+      setRefetch(prev => !prev);
+      setContador(0);
+    } else {
+      setContador(contador + 1)
+    }
+  }
 
   return (
     <div>
       <p>{quotes[contador].text}</p>
       <h3>{quotes[contador].author}</h3>
-      <button onClick={() => setContador(contador + 1)}>Siguiente</button>
+      <button onClick={handleClick}>Siguiente</button>
       <button>Agregar</button>
     </div>
   );
