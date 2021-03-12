df7a3980cedda1822e668334b6417f275b4b5a8d
Franco Dall'Ongaro <francodallongaro@gmail.com>
2021-03-12 09:24:07 -0300

    Ultima actualizacion

--- src/components/inspiracion/index.js
+++ src/components/inspiracion/index.js
@@ -3,10 +3,24 @@ import Quotes from "../quotes";
 import SidebarQuotes from "../SidebarQuotes";
 
 function Inspiracion() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://goquotes-api.herokuapp.com/api/v1/random?count=10")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuotes(data.quotes);
+        setLoading(false);
+        console.log("quotes", quotes);
+      });
+  }, []);
+
+  if (loading) <h1>Loading</h1>;
   return (
     <div className="layout">
       <SidebarQuotes />
-      <Quotes />
+      <Quotes quotes={quotes} />
     </div>
   );
 }
--- src/components/quotes/index.js
+++ src/components/quotes/index.js
@@ -1,20 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-function Quotes() {
-  const [quotes, setQuotes] = useState([]);
+function Quotes({ quotes }) {
   const [contador, setContador] = useState(0);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetch("https://goquotes-api.herokuapp.com/api/v1/random?count=10")
-      .then((res) => res.json())
-      .then((data) => {
-        setQuotes(data.quotes);
-        setLoading(false);
-        console.log("quotes", quotes);
-      });
-  }, []);
 
-  if (loading) <h1>Loading</h1>;
   return (
     <div>
       <p>{quotes[contador].text}</p>
